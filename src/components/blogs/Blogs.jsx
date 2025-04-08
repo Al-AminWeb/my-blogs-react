c98d82faba75f9fddf52235fb5e44813328fb413
Al-AminWeb <143194944+Al-AminWeb@users.noreply.github.com>
2025-04-08 22:00:55 +0600

    card added and dynamically the data has shown

--- src/components/blogs/Blogs.jsx
+++ src/components/blogs/Blogs.jsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import Blog from "../blog/Blog.jsx";
 
 const Blogs = () => {
 const [blogs, setBlogs] = useState([]);
@@ -14,7 +15,11 @@ const [blogs, setBlogs] = useState([]);
     return (
         <div>
             <h1 className='text-3xl'>Total Blogs: {blogs.length}</h1>
-
+    <div className='all-blogs grid grid-cols-2 gap-4'>
+        {
+            blogs.map((blog) => <Blog blog={blog}></Blog>)
+        }
+    </div>
         </div>
     );
 };
