392d5256586faae914b2e865e11d3c603d09cd99
Al-AminWeb <143194944+Al-AminWeb@users.noreply.github.com>
2025-04-08 21:44:11 +0600

    json data added and fetched and the how many blogs are there is shown

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Navbar from "./components/navbar/Navbar.jsx";
+import Blogs from "./components/blogs/Blogs.jsx";
 
 function App() {
 
@@ -8,6 +9,18 @@ function App() {
   return (
     <>
       <Navbar></Navbar>
+
+        <div className="main-container flex text-center">
+            <div className="left-container w-[70%]">
+
+
+                <Blogs></Blogs>
+            </div>
+            <div className="right-container w-[30%] border-2">
+                <h1>Reading time:0</h1>
+                <h1>Bookmark Count:0</h1>
+            </div>
+        </div>
     </>
   )
 }
