ae784c1291de114bb5078d1a665a8fd772cdf380
Al-AminWeb <143194944+Al-AminWeb@users.noreply.github.com>
2025-04-09 00:17:32 +0600

    add time when marked as read

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,39 @@
-
 import './App.css'
 import Navbar from "./components/navbar/Navbar.jsx";
 import Blogs from "./components/blogs/Blogs.jsx";
+import {useState} from "react";
 
 function App() {
 
+    const [bookMark, setBookMark] = useState([]);
+    const[readingCount, setReadingCount] = useState(0);
+    const handleBookMark = (blog) => {
+        setBookMark([...bookMark, blog]);
+    }
 
-  return (
-    <>
-      <Navbar></Navbar>
+    const handleMarkAsRead = (time) => {
+        setReadingCount(readingCount + time);
+    }
+    console.log(readingCount);
+    return (
+        <>
+            <Navbar></Navbar>
 
-        <div className="main-container flex text-center">
-            <div className="left-container w-[70%]">
+            <div className="main-container flex text-center">
+                <div className="left-container w-[70%]">
 
 
-                <Blogs></Blogs>
+                    <Blogs handleBookMark={handleBookMark} handleMarkAsRead={handleMarkAsRead}></Blogs>
+                </div>
+                <div className="right-container w-[30%] border-2">
+                    <h1>Reading time:{readingCount}</h1>
+                    <h1>Bookmark Count:{bookMark.length}</h1>
+
+                    {bookMark.map((marked) => <p>{marked.title}</p>)}
+                </div>
             </div>
-            <div className="right-container w-[30%] border-2">
-                <h1>Reading time:0</h1>
-                <h1>Bookmark Count:0</h1>
-            </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default App
--- src/components/blogs/Blogs.jsx
+++ src/components/blogs/Blogs.jsx
@@ -1,25 +1,24 @@
 import React, {useEffect, useState} from 'react';
 import Blog from "../blog/Blog.jsx";
 
-const Blogs = () => {
-const [blogs, setBlogs] = useState([]);
+const Blogs = ({handleBookMark,handleMarkAsRead}) => {
+    const [blogs, setBlogs] = useState([]);
 
     useEffect(() => {
         fetch("blogs.json")
-        .then(res => res.json())
-        .then(data => setBlogs(data))
+            .then(res => res.json())
+            .then(data => setBlogs(data))
     }, []);
 
-    console.log(blogs);
 
     return (
         <div>
             <h1 className='text-3xl'>Total Blogs: {blogs.length}</h1>
-    <div className='all-blogs grid grid-cols-2 gap-4'>
-        {
-            blogs.map((blog) => <Blog blog={blog}></Blog>)
-        }
-    </div>
+            <div className='all-blogs grid grid-cols-2 gap-4'>
+                {
+                    blogs.map((blog) => <Blog blog={blog} handleBookMark={handleBookMark} handleMarkAsRead={handleMarkAsRead}></Blog>)
+                }
+            </div>
         </div>
     );
 };
