6dd263a051deacf1f4f3eda7279a0218648b9d22
Al-AminWeb <143194944+Al-AminWeb@users.noreply.github.com>
2025-04-08 20:22:37 +0600

    Project Initialized

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Navbar from "./components/navbar/Navbar.jsx";
+
+function App() {
+
+
+  return (
+    <>
+      <Navbar></Navbar>
+    </>
+  )
+}
+
+export default App
